fba09a706a60a6f6f714dfe490fc917dd3da9e16
Joe Passafiume <joepass456@gmail.com>
2020-09-24 15:09:56 -0400

    version 1.2

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
-// Start Program in state 0
-//const topText = document.getElementById("topText").innerHTML;
-//const botText = document.getElementById("botText").innerHTML;
+//  Link to HTML elements
 const nextBut = document.querySelector('.nextBut');
 const rstBut = document.querySelector('.rstBut');
 const topText = document.querySelector('.topText');
 const botText = document.querySelector('.botText');
+//  Init variables
 var state = 0;
+//  Randomize symbol for x * 9s
 var symbol = Math.floor(Math.random() * 9);
 
-//mndRdr object declared
+// Declare data object
 const data = {
     bgTxt: ['Welcome to the MIND-READER',
         'Visualize a number from 01 to 99',
         'Add each of the the two digits of your number together',
         'Now subtract that sum from the original number',
         '(symbol scroll)',
-        'Look at the blank screen and visualize the symbol',
+        'Stare at the screen and visualize the symbol in your mind',
         '(show symbol)',
         'I hope you were sitting down!'],
     smlTxt: ['Prepare to be amazed!',
-        'Once you see it press the button',
+        'Once you see it press the next button',
         'If you chose 92, 9 + 2',
         '92 minus 11',
         'Scroll down and find the symbol for your new number',
@@ -29,10 +29,11 @@ const data = {
     smbls: ['~', '!', '@', '#', '$', '%', '+', '&', '*'],
 
 }
+// ****  START PROGRAM *****
 document.body.onload = mndRdr(0);
 
+// Main Program
 function mndRdr() {
-    //console.log(state, data.bgTxt[state]);
     if (state == 8) {rstnow()}
     if (data.bgTxt[state] == "(symbol scroll)") {
         topText.innerHTML = makeSymbolString();
@@ -46,6 +47,7 @@ function mndRdr() {
     state++;
 }
 
+//  Create the string of number : symbol
 function makeSymbolString() {
     var symbolString = "";
     var s = symbol;
@@ -59,7 +61,7 @@ function makeSymbolString() {
 }
 
 
-
+//  Reset
 function rstnow() {
     state = 0;
     symbol = Math.floor((Math.random() * 9) + 1);
@@ -67,6 +69,8 @@ function rstnow() {
     //nextBut.addEventListener('click', mndRdr);
     //mndRdr(0);
 }
+
+//  Button Listeners
 nextBut.addEventListener('click', mndRdr);
 rstBut.addEventListener('click', rstnow);
 
